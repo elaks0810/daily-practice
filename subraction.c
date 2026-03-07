4ec952e9eb360ab71851af460623b3639fd409c8
elaks0810 <elakkya082005@gmail.com>
2026-03-07 20:35:09 +0530

    Added C practice programs

---
+++ subraction.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    int num1, num2, diff;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    diff = num1 - num2;
+    printf("Difference: %d", diff);
+    return 0;
+}
